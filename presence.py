6be91af02dd86485c33c56674f46910c706cb54a
André <afcavalcanti99@gmail.com>
2017-09-11 19:34:28 -0300

    saving json file

--- presence.py
+++ presence.py
@@ -1,21 +1,12 @@
 import subprocess
-from time import sleep
+from time import sleep, strftime, time
 from threading import Thread
-from ISStreamer.Streamer import Streamer
 
 # Edit these for how many people/devices you want to track
-occupant = ["Rachel","Jamie","Broc","Adam","Jeff","Raymond","David","Kaylee"]
+occupant = ["Andre"]
 
 # MAC addresses for our phones
-address = ["xx:xx:xx:xx:xx:xx","xx:xx:xx:xx:xx:xx","xx:xx:xx:xx:xx:xx","xx:xx:xx:xx:xx:xx","xx:xx:xx:xx:xx:xx","xx:xx:xx:xx:xx:xx","xx:xx:xx:xx:xx:xx","xx:xx:xx:xx:xx:xx"]
-
-# Sleep once right when this script is called to give the Pi enough time
-# to connect to the network
-sleep(60)
-
-# Initialize the Initial State streamer
-# Be sure to add your unique access key
-streamer = Streamer(bucket_name=":office:Who's at the Office?", bucket_key="office_presence", access_key="Your_Access_Key")
+address = ["b0:72:bf:87:47:9d"]
 
 # Some arrays to help minimize streaming and account for devices
 # disappearing from the network when asleep
@@ -24,15 +15,22 @@ presentSent = [0] * len(occupant)
 notPresentSent = [0] * len(occupant)
 counter = [0] * len(occupant)
 
+def write_presence(occupant):
+    with open("presence.json", "a") as log:
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(occupant)))
+        
+def write_address(output):
+    with open("mac.json", "a") as log:
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(output)))
+
 # Function that checks for device presence
 def whosHere(i):
-
-    # 30 second pause to allow main thread to finish arp-scan and populate output
-    sleep(30)
+    
+    sleep(15)
 
     # Loop through checking for devices and counting if they're not present
     while True:
-
+        
         # Exits thread if Keyboard Interrupt occurs
         if stop == True:
             print "Exiting Thread"
@@ -44,21 +42,15 @@ def whosHere(i):
         if address[i] in output:
             print(occupant[i] + "'s device is connected to your network")
             if presentSent[i] == 0:
-                # Stream that device is present
-                streamer.log(occupant[i],":office:")
-                streamer.flush()
-                print(occupant[i] + " present streamed")
-                # Reset counters so another stream isn't sent if the device
-                # is still present
                 firstRun[i] = 0
                 presentSent[i] = 1
                 notPresentSent[i] = 0
                 counter[i] = 0
-                sleep(900)
+                sleep(60)
             else:
-                # If a stream's already been sent, just wait for 15 minutes
+                # If a stream's already been sent, just wait for 30 seconds
                 counter[i] = 0
-                sleep(900)
+                sleep(60)
         # If a listed device address is not present, print and stream
         else:
             print(occupant[i] + "'s device is not present")
@@ -67,25 +59,19 @@ def whosHere(i):
             if counter[i] == 30 or firstRun[i] == 1:
                 firstRun[i] = 0
                 if notPresentSent[i] == 0:
-                    # Stream that device is not present
-                    streamer.log(occupant[i],":no_entry_sign::office:")
-                    streamer.flush()
-                    print(occupant[i] + " not present streamed")
-                    # Reset counters so another stream isn't sent if the device
-                    # is still present
                     notPresentSent[i] = 1
                     presentSent[i] = 0
                     counter[i] = 0
                 else:
                     # If a stream's already been sent, wait 30 seconds
                     counter[i] = 0
-                    sleep(30)
+                    sleep(60)
             # Count how many 30 second intervals have happened since the device 
             # disappeared from the network
             else:
                 counter[i] = counter[i] + 1
                 print(occupant[i] + "'s counter at " + str(counter[i]))
-                sleep(30)
+                sleep(60)
 
 
 # Main thread
@@ -107,9 +93,11 @@ try:
         global output
         # Assign list of devices on the network to "output"
         output = subprocess.check_output("sudo arp-scan -l", shell=True)
-        # Wait 30 seconds between scans
-        sleep(30)
-
+        write_presence(occupant)
+        write_address(output)
+        
+        # Wait 60 seconds between scans
+        sleep(60)
 except KeyboardInterrupt:
     # On a keyboard interrupt signal threads to exit
     stop = True
